6e1e98afa66fa78d454b20a1e6611b94b9213aa6
Nikolai Shabalin <dev.nikolai.shabalin@gmail.com>
2025-04-10 12:18:01 +0300

    Fix path

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://shabalin.online/',
-	base: '/',
+	site: 'https://shabalin.online',
+	base: '',
 	integrations: [mdx(), sitemap()]
 });
