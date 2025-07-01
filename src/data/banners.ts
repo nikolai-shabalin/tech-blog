2d54fbbba3a2cc2d6a8c5d290a0e759cc3e2f51c
Nikolai Shabalin <dev.nikolai.shabalin@gmail.com>
2025-07-01 20:35:43 +0300

    Adds digest #6

--- src/data/banners.ts
+++ src/data/banners.ts
@@ -7,4 +7,8 @@ export const banners = [
     image: '/images/banners/pletnev.jpg',
     portfolioUrl: 'https://spb.hh.ru/resume/1e09b0f3ff0ef080f40039ed1f4b78384f5a73'
   },
-]; 
+  {
+    image: '/images/banners/ivanov-sergei.png',
+    portfolioUrl: 'https://spb.hh.ru/resume/1e09b0f3ff0ef080f40039ed1f4b78384f5a73'
+  },
+];
