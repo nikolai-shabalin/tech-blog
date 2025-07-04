83421268c32d915dc930db0ca68c8497b442e263
Nikolai Shabalin <dev.nikolai.shabalin@gmail.com>
2025-07-05 00:03:42 +0300

    Adds new banner

--- src/data/banners.ts
+++ src/data/banners.ts
@@ -11,4 +11,8 @@ export const banners = [
     image: '/images/banners/ivanov-sergei.png',
     portfolioUrl: 'https://spb.hh.ru/resume/1e09b0f3ff0ef080f40039ed1f4b78384f5a73'
   },
+  {
+    image: '/images/banners/petrushin.png',
+    portfolioUrl: 'https://bryansk.hh.ru/resume/bd32116dff0e988b470039ed1f3339324e6d59'
+  },
 ];
